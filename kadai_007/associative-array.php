261aa8ee53d5d1722630406ddfb174f3a94969e5
yuzu202408 <akebon1321@gmail.com>
2024-09-26 22:32:44 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <P>
+    <?php
+    $personal_data=['name'=>'onion','price'=>'200','weight'=>'160'] ;
+    print_r($personal_data);
+
+    ?>
+    
+    
+
+    </p>
+
+</body>
+
+</html>
